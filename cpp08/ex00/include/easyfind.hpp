a4c799c658c8a68c62fc7fe5027289bf48aa21e7
Ulysse Clement <ulysse@ulyssepc.home>
2024-03-10 16:48:36 +0100

    push ex00

--- cpp08/ex00/include/easyfind.hpp
+++ cpp08/ex00/include/easyfind.hpp
@@ -3,25 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: uclement <uclement@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ulysse <ulysse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 15:47:15 by ulysse            #+#    #+#             */
-/*   Updated: 2024/03/08 10:29:50 by uclement         ###   ########.fr       */
+/*   Updated: 2024/03/10 16:46:07 by ulysse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
-#include "Color.hpp"
 #include <iostream>
+#include <exception>
+#include <vector>
+#include <list>
+#include <deque>
+#include <algorithm>
+#include <iterator>
+
+
+
+class EasyFindException : public std::exception {
+	public:
+		const char *what() const throw() {
+			return "Exception: number not found in container";
+   		}
+};
 
 template <typename T>
-void easyfind (T a, int b)
+int easyfind (const T& container, int toFind)
 {
-	givenType tmp = a;
-	a = b;
-	b = tmp;
+	typename T::const_iterator it = std::find(container.begin(), container.end(), toFind);
+	if (it != container.end())
+		return (std::distance(container.begin(),it));
+	else
+		throw EasyFindException();
 }
 
 #endif
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,68 +6,82 @@
 /*   By: ulysse <ulysse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 14:23:19 by uclement          #+#    #+#             */
-/*   Updated: 2024/03/04 16:45:02 by ulysse           ###   ########.fr       */
+/*   Updated: 2024/03/10 16:47:54 by ulysse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Whatever.hpp"
-#include <string>
-
-
-class Test
-{
-    public:
-        Test( int n ) : _n(n) {}
-        bool operator==( Test const & rhs ) const { return (this->_n == rhs._n); }
-        bool operator!=( Test const & rhs ) const { return (this->_n != rhs._n); }
-        bool operator>( Test const & rhs ) const { return (this->_n > rhs._n); }
-        bool operator<( Test const & rhs ) const { return (this->_n < rhs._n); }
-        bool operator>=( Test const & rhs ) const { return (this->_n >= rhs._n); }
-        bool operator<=( Test const & rhs ) const { return (this->_n <= rhs._n); }
-        int get( void ) const { return this->_n; }
-    private:
-        int _n;
-};
-
-std::ostream & operator<<( std::ostream & os, Test const & rhs ) { os << rhs.get(); return os; }
+#include "easyfind.hpp"
 
 int main()
-{
-	{
-	int a = 2;
-	int b = 3;
-	swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-	std::string c = "chaine1";
-	std::string d = "chaine2";
-	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-	}
-	{
-	Test a(2), b(4), c(7);
-    if (a != c)
-        std::cout << "a != c" << std::endl;
-    a = c;
-    if (a == c)
-        std::cout << "a == c" << std::endl;
-    if (a > b)
-        std::cout << "a > b" << std::endl;
-    if (a < b)
-        std::cout << "a < b" << std::endl;
-    if (b >= a)
-        std::cout << "b >= a" << std::endl;
-    if (b <= a)
-        std::cout << "b <= a" << std::endl;
-    std::cout << "a = " << a << ", b = " << b << std::endl;
-    swap<Test>(a, b);
-    std::cout << "a = " << a << ", b = " << b << std::endl;
-    std::cout << "max( a, b ) = " << max<Test>( a, b ) << std::endl;
-    std::cout << "min( a, b ) = " << min<Test>( a, b ) << std::endl;
-	}
-}
+{  
+    std::vector<int> test;
 
+    test.push_back(10);
+    test.push_back(20);
+    test.push_back(30);
+
+    try 
+    {
+        std::cout <<  ::easyfind(test, 10) << " <- Position of the element in the container" << std::endl;
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    try 
+    {
+        std::cout <<  ::easyfind(test, 11) << " <- Position of the element in the container" << std::endl;
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    
+
+    std::list<int> lst;
+
+    lst.push_back(10);
+    lst.push_back(20);
+    lst.push_back(30);
+
+    try 
+    {
+        std::cout <<  ::easyfind(lst, 20) << " <- Position of the element in the container" << std::endl;
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    try 
+    {
+        std::cout <<  ::easyfind(lst, 11) << " <- Position of the element in the container" << std::endl;
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    std::deque<int> deque;
+
+    deque.push_back(10);
+    deque.push_back(20);
+    deque.push_back(30);
+
+    try 
+    {
+        std::cout <<  ::easyfind(deque, 30) << " <- Position of the element in the container" << std::endl;
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    try 
+    {
+        std::cout <<  ::easyfind(deque, 11) << " <- Position of the element in the container" << std::endl;
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+}
 
