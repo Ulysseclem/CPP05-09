b5076600945cc008cc90d34d026560b10464f408
Ulysse CLEMENT <uclement@bess-f1r1s12.clusters.42paris.fr>
2024-03-12 17:23:43 +0100

    push

--- cpp09/ex00/src/BitcoinExchange.cpp
+++ cpp09/ex00/src/BitcoinExchange.cpp
@@ -1,39 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: uclement <uclement@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/23 14:23:19 by uclement          #+#    #+#             */
-/*   Updated: 2024/03/12 17:22:48 by uclement         ###   ########.fr       */
+/*   Created: 2024/03/10 16:58:58 by ulysse            #+#    #+#             */
+/*   Updated: 2024/03/12 17:07:29 by uclement         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <cstdlib>
-#include <iostream>
-#include <fstream> 
-#include <exception> 
-
-
-void errorExit(std::string msg)
-{
-	std::cerr << msg << std::endl;
-	exit(EXIT_FAILURE);
-}
-
-
-int main(int ac, char **av)
-{  
-	if (ac != 2)
-		errorExit ("Wrong number of arguments");
-		
-	std::ifstream file;
-	file.open(av[1]);
-	if(!file.is_open())
-		errorExit ("error with opening input");
-	
-	
-	exit(EXIT_SUCCESS);
-}
-
+#include "Span.hpp"
